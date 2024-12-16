526470ff30d67d55d8a9be958bcdfc0505f9c280
Alexander <vsany@meta.ua>
2024-12-16 15:02:58 +0200

    in work: correct display cards current experience for sliders

---
+++ Assets/Scripts/Services/ExperienceService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TMPro;
+using UnityEngine.UI;
+
+namespace Services
+{
+    public class ExperienceService : Singleton<ExperienceService>
+    {
+        public Slider Card1ExpSlider;
+        public Slider Card2ExpSlider;
+        public Slider Card3ExpSlider;
+        public Slider Card4ExpSlider;
+        public Slider Card5ExpSlider;
+
+        public TMP_Text Card1TextOnSlider;
+        public TMP_Text Card2TextOnSlider;
+        public TMP_Text Card3TextOnSlider;
+        public TMP_Text Card4TextOnSlider;
+        public TMP_Text Card5TextOnSlider;
+        
+        private float card1CurrentExp;
+        private float card2CurrentExp;
+        private float card3CurrentExp;
+        private float card4CurrentExp;
+        private float card5CurrentExp;
+
+
+
+        private void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/Singelton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    static protected T Instance { get; set; }
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = (T) (object) this;
+        }
+    }
+}
