0d9146ab1c39cf81fc88467d3e268cc4f04df54d
Alexander <vsany@meta.ua>
2024-12-30 12:46:40 +0200

    [+] toggle inventory by click and by press I

---
+++ Assets/Scripts/Inventory/InventoryOpenAndClose.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace Inventory
+{
+    public class InventoryOpenAndClose : MonoBehaviour
+    {
+        public GameObject InventoryCanvas;
+
+        private bool _isOpenedInventory;
+
+        public void Awake()
+        {
+            _isOpenedInventory = false;
+        }
+
+        public void ToggleInventory()
+        {
+            if (_isOpenedInventory)
+            {
+                _onCloseInventory();
+            }
+            else
+            {
+                _onOpenInventory();
+            }
+        }
+
+        
+        private void _onOpenInventory()
+        {
+            InventoryCanvas.SetActive(true);
+            _isOpenedInventory = true;
+        }
+
+        
+        private void _onCloseInventory()
+        {
+            InventoryCanvas.SetActive(false);
+            _isOpenedInventory = false;
+        }
+
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                ToggleInventory();
+            }
+        }
+    }
+}
