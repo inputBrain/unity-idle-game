7ef5da7a4d7491bd47771829b2ef75e6e590bfc6
Alexander <vsany@meta.ua>
2025-05-23 18:16:59 +0300

    refactor(InventoryPresenter): delete HandleItemClicked and HandleItemDeleteClicked methods

--- Assets/Scripts/Presentation/Inventory/InventoryPresenter.cs
+++ Assets/Scripts/Presentation/Inventory/InventoryPresenter.cs
@@ -13,7 +13,6 @@ namespace Presentation.Inventory
     public class InventoryPresenter : Singleton<InventoryPresenter>, IDisposable
     {
         private readonly InventoryModel _inventoryModel;
-        private readonly InventoryView _inventoryView;
         private readonly Dictionary<string, Sprite> _spriteCache = new();
 
         private readonly Sprite _defaultIcon;
@@ -23,13 +22,9 @@ namespace Presentation.Inventory
         public InventoryPresenter(InventoryModel model, InventoryView view, Sprite defaultIcon = null)
         {
             _inventoryModel = model ?? throw new ArgumentNullException(nameof(model));
-            _inventoryView = view ?? throw new ArgumentNullException(nameof(view));
             _defaultIcon = defaultIcon;
 
             _inventoryModel.OnInventoryChanged += HandleInventoryChanged;
-            _inventoryView.OnItemClicked += HandleItemClicked;
-            _inventoryView.OnItemDeleteClicked += HandleItemDeleteClicked;
-
             HandleInventoryChanged();
         }
 
@@ -42,21 +37,8 @@ namespace Presentation.Inventory
                     var icon = LoadSprite(iconPath);
                     return new EntityItem(domainItem, icon);
                 }).ToList();
-
-            _inventoryView.DisplayItems(itemsToDisplay);
         }
-
-        private void HandleItemClicked(IInventoryItem clickedItem)
-        {
-            Debug.Log($"InventoryPresenter: Кликнул: {clickedItem.Title}");
-            _inventoryModel.ToggleSelection(clickedItem);
-        }
-
-        private void HandleItemDeleteClicked(IInventoryItem itemToDelete)
-        {
-            Debug.Log($"InventoryPresenter: Запрос на удаление: {itemToDelete.Title}");
-            _inventoryModel.RemoveItem(itemToDelete);
-        }
+        
 
         private Sprite LoadSprite(string path)
         {
@@ -78,11 +60,6 @@ namespace Presentation.Inventory
         {
             if (_inventoryModel != null)
                 _inventoryModel.OnInventoryChanged -= HandleInventoryChanged;
-            if (_inventoryView != null)
-            {
-                _inventoryView.OnItemClicked -= HandleItemClicked;
-                _inventoryView.OnItemDeleteClicked -= HandleItemDeleteClicked;
-            }
             _spriteCache.Clear();
         }
 
