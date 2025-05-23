da6dd7c2d6f660ee0311e38956a8c74e73ffc594
Alexander <vsany@meta.ua>
2025-05-23 18:19:04 +0300

    refactor: remove card rendering responsibilities from ToolbarView
    - Moved DisplayToolbarCards logic to presenter layer
    - ToolbarView now exposes only ClearAllItems and SpawnEntityView methods
    - Promotes single responsibility and testability

--- Assets/Scripts/Presentation/Toolbar/ToolbarView.cs
+++ Assets/Scripts/Presentation/Toolbar/ToolbarView.cs
@@ -10,57 +10,16 @@ namespace Presentation.Toolbar
 {
     public class ToolbarView : MonoBehaviour
     {
-        [SerializeField] private Transform toolbarContainerGrid;
-        [SerializeField] private GameObject itemSlotPrefab;
+        [SerializeField] private Transform toolbarContainer;
+        [SerializeField] private EntityView itemPrefab;
 
-        private readonly List<EntityView> _currentToolbarSlots = new();
-
-        // На событие подписывается ToolbarPresenter
-        public event Action<IInventoryItem> OnToolbarItemDropped;
-
-        public void DisplayToolbarCards(IReadOnlyList<CardModel> cards)
+        public void ClearAllItems()
         {
-            ClearToolbar();
-
-            foreach (var card in cards)
-            {
-                if (card == null) continue;
-
-                GameObject slotInstance = Instantiate(itemSlotPrefab, toolbarContainerGrid);
-                var itemView = slotInstance.GetComponent<EntityView>();
-                if (itemView != null)
-                {
-                    var item = new EntityItem(card, LoadSprite(card.IconResourcesPath.Value));
-                    itemView.Init(item, isToolbar: true);
-                    _currentToolbarSlots.Add(itemView);
-                }
-                else
-                {
-                    Debug.LogError("Префаб слота не содержит ItemView.");
-                    Destroy(slotInstance);
-                }
-            }
+            foreach (Transform t in toolbarContainer) Destroy(t.gameObject);
         }
 
-        private void ClearToolbar()
-        {
-            foreach (var slot in _currentToolbarSlots)
-            {
-                if (slot != null)
-                    Destroy(slot.gameObject);
-            }
-            _currentToolbarSlots.Clear();
-        }
-
-        private void HandleToolbarSlotClick(IInventoryItem clickedItem)
-        {
-            OnToolbarItemDropped?.Invoke(clickedItem);
-        }
-
-        private Sprite LoadSprite(string path)
-        {
-            if (string.IsNullOrEmpty(path)) return null;
-            return Resources.Load<Sprite>(path);
-        }
+        public EntityView SpawnItemView()
+            => Instantiate(itemPrefab, toolbarContainer, false);
     }
+
 }
