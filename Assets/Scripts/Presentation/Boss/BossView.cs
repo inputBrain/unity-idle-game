acd12a9d0aa2dff2e4cc03bca83e45d1a04a9af8
inputBrain <vsanyinclude@gmail.com>
2025-06-04 19:26:48 +0300

    Animate HP sliders

--- Assets/Scripts/Presentation/Boss/BossView.cs
+++ Assets/Scripts/Presentation/Boss/BossView.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Collections;
+using System.Globalization;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,11 +13,36 @@ namespace Presentation.Boss
         public TMP_Text HpOnSlider;
 
 
+        [SerializeField]
+        private float _sliderAnimDuration = 0.5f;
+
+        private Coroutine _sliderRoutine;
+
         public void SetSliderHp(float bossMaxHp, float bossCurrentHp)
         {
-            Slider.value = bossCurrentHp / bossMaxHp;
+            var target = bossCurrentHp / bossMaxHp;
 
             HpOnSlider.text = ((int)bossCurrentHp).ToString(CultureInfo.InvariantCulture);
+
+            if (_sliderRoutine != null)
+                StopCoroutine(_sliderRoutine);
+
+            _sliderRoutine = StartCoroutine(AnimateSlider(target));
+        }
+
+        private IEnumerator AnimateSlider(float target)
+        {
+            var start = Slider.value;
+            var time = 0f;
+
+            while (time < _sliderAnimDuration)
+            {
+                time += Time.deltaTime;
+                Slider.value = Mathf.Lerp(start, target, time / _sliderAnimDuration);
+                yield return null;
+            }
+
+            Slider.value = target;
         }
     }
 }
