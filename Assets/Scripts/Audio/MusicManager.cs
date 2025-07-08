c292f1306e2b7a5ab4aa42f28460f3cdffe507a0
Oleksandr Voznyi <vsanyinclude@gmail.com>
2025-07-08 16:14:53 +0300

    feat(audio): add music manager to load random music file

---
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Audio
+{
+    public class MusicManager : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource audioSource;
+
+        [SerializeField]
+        private List<AudioClip> musicClips = new List<AudioClip>();
+
+
+        private void Awake()
+        {
+            if (FindObjectsOfType<MusicManager>().Length > 1)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            DontDestroyOnLoad(gameObject);
+
+            if (audioSource == null)
+            {
+                audioSource = GetComponent<AudioSource>();
+            }
+        }
+
+
+        private void Start()
+        {
+            if (musicClips == null || musicClips.Count == 0 || audioSource == null)
+            {
+                return;
+            }
+
+            var clip = musicClips[Random.Range(0, musicClips.Count)];
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+    }
+}
